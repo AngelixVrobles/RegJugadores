fb9eada161aa42c44504fba5af414aa9897fb05d
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-09-08 15:58:56 -0700

    Creación de la entidad Jugador para la base de datos local.
    Se define la estructura de la tabla `jugadores` con los campos `jugadorId` (clave primaria autogenerada), `nombres` (String) y `partidas` (Int).

---
+++ app/src/main/java/com/example/regjugadores/data/local/Jugador.kt
@@ -0,0 +1,11 @@
+package com.example.regjugadores.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jugadores")
+data class Jugador(
+    @PrimaryKey(autoGenerate = true) val jugadorId: Int = 0,
+    val nombres: String,
+    val partidas: Int
+)
