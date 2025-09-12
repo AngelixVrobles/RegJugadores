3d9bc8991a8cf61a9a077d397e5c7461242441e6
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-09-12 16:58:43 -0700

    Actualizacion de JugadorDatabase.kt

--- app/src/main/java/com/example/regjugadores/data/local/JugadorDatabase.kt
+++ app/src/main/java/com/example/regjugadores/data/local/JugadorDatabase.kt
@@ -1,9 +1,28 @@
 package com.example.regjugadores.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Jugador::class], version = 1)
+@Database(entities = [Jugador::class], version = 1, exportSchema = false)
 abstract class JugadorDatabase : RoomDatabase() {
     abstract fun jugadorDao(): JugadorDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: JugadorDatabase? = null
+
+        fun getDatabase(context: Context): JugadorDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    JugadorDatabase::class.java,
+                    "jugador_db"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
