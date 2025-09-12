812b1615fe8266b1105462d908b06ad37f31e212
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-09-12 16:57:53 -0700

    Refactorización de la entidad Jugador y actualización de la configuración de Gradle.
    Se han renombrado los campos en la entidad `Jugador` de `nombres` a `nombreJugador` y de `partidas` a `partidasJugadas`, asignando a este último un valor por defecto de 0. Se especificaron los nombres de las columnas en la base de datos mediante la anotación `@ColumnInfo`.
    
    Adicionalmente, se actualizó la versión del JDK de Gradle a `temurin-17` en la configuración del proyecto.

--- app/src/main/java/com/example/regjugadores/data/local/Jugador.kt
+++ app/src/main/java/com/example/regjugadores/data/local/Jugador.kt
@@ -1,11 +1,12 @@
 package com.example.regjugadores.data.local
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "jugadores")
 data class Jugador(
     @PrimaryKey(autoGenerate = true) val jugadorId: Int = 0,
-    val nombres: String,
-    val partidas: Int
+    @ColumnInfo(name = "nombre_jugador") val nombreJugador: String,
+    @ColumnInfo(name = "partidas_jugadas") val partidasJugadas: Int = 0
 )
