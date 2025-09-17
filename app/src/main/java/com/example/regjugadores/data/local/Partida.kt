f9e696f68543798e27f3cae2db59af7bf9f423d9
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-09-17 13:07:12 -0700

    Eliminación de valores por defecto en la entidad `Partida`.
    Se han eliminado los valores predeterminados para las propiedades `fecha` y `esFinalizada` en la clase de datos `Partida`.
    
    A partir de ahora, es obligatorio proporcionar explícitamente estos valores al crear una instancia de `Partida`, asegurando que cada registro tenga una fecha y un estado de finalización definidos intencionadamente.
    
    También se ha realizado un ajuste menor en el comentario del campo `ganadorId` para mayor claridad.
    
    Signed-off-by: AngelixVrobles <angelixvrobles1234@outlook.com>

--- app/src/main/java/com/example/regjugadores/data/local/Partida.kt
+++ app/src/main/java/com/example/regjugadores/data/local/Partida.kt
@@ -6,9 +6,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "partidas")
 data class Partida(
     @PrimaryKey(autoGenerate = true) val partidaId: Int = 0,
-    val fecha: Long = System.currentTimeMillis(), // guardamos en timestamp
+    val fecha: Long,
     val jugador1Id: Int,
     val jugador2Id: Int,
-    val ganadorId: Int?, // puede ser null si hay empate
-    val esFinalizada: Boolean = false
+    val ganadorId: Int?,  // null si es empate
+    val esFinalizada: Boolean
 )
