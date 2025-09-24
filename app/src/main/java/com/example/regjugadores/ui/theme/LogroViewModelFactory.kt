00579434f8c7c88b3e015a61aef787e5363149aa
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-09-24 15:02:13 -0700

    Creación de LogroViewModel y LogroViewModelFactory.
    Se implementa `LogroViewModel` para gestionar la lógica de negocio de los logros, incluyendo funciones para obtener, registrar y eliminar logros asociados a un jugador. Se añade `LogroViewModelFactory` para la creación de instancias de `LogroViewModel`.

---
+++ app/src/main/java/com/example/regjugadores/ui/theme/LogroViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.regjugadores.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.regjugadores.data.repository.LogroRepository
+
+class LogroViewModelFactory(
+    private val repository: LogroRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LogroViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return LogroViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
