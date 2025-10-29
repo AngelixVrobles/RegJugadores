ba9d6852fbdd7bfe8149fed3a3a33d8f4eef164b
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-10-29 14:12:58 -0700

    feat: Habilita consultas en el hilo principal y actualiza la base de datos
    Se añade `.allowMainThreadQueries()` a la construcción de la base de datos en `JugadorDatabase.kt` para permitir la ejecución de consultas de Room en el hilo principal. Se eliminaron comentarios y código innecesarios para mejorar la legibilidad.

--- app/src/main/java/com/example/regjugadores/data/local/JugadorDatabase.kt
+++ app/src/main/java/com/example/regjugadores/data/local/JugadorDatabase.kt
@@ -9,30 +9,35 @@ import androidx.room.RoomDatabase
     entities = [
         Jugador::class,
         Partida::class,
-        Logro::class // 👈 añadimos la entidad Logro
+        Logro::class
     ],
-    version = 4, // 👈 incrementa cada vez que cambies entidades
+    version = 4,
     exportSchema = false
 )
 abstract class JugadorDatabase : RoomDatabase() {
 
     abstract fun jugadorDao(): JugadorDao
     abstract fun partidaDao(): PartidaDao
-    abstract fun logroDao(): LogroDao // 👈 nuevo DAO
+    abstract fun logroDao(): LogroDao
 
     companion object {
         @Volatile
         private var INSTANCE: JugadorDatabase? = null
 
         fun getDatabase(context: Context): JugadorDatabase {
+
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     JugadorDatabase::class.java,
                     "jugador_db"
                 )
-                    .fallbackToDestructiveMigration() // 👈 elimina DB vieja si cambian entidades
+
+                    .fallbackToDestructiveMigration()
+
+                    .allowMainThreadQueries()
                     .build()
+
                 INSTANCE = instance
                 instance
             }
