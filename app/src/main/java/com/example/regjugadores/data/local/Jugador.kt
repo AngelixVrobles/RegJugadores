9eb41c01fd1bf75b254cc6329bd0146c02e01e97
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-10-29 14:12:42 -0700

    Actualización de la entidad `Jugador`.
    Se elimina el campo `partidasJugadas` y las anotaciones `@ColumnInfo`. Se añade el campo `email` a la entidad `Jugador`.

--- app/src/main/java/com/example/regjugadores/data/local/Jugador.kt
+++ app/src/main/java/com/example/regjugadores/data/local/Jugador.kt
@@ -1,12 +1,12 @@
 package com.example.regjugadores.data.local
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "jugadores")
 data class Jugador(
-    @PrimaryKey(autoGenerate = true) val jugadorId: Int = 0,
-    @ColumnInfo(name = "nombre_jugador") val nombreJugador: String,
-    @ColumnInfo(name = "partidas_jugadas") val partidasJugadas: Int = 0
+    @PrimaryKey(autoGenerate = true)
+    val jugadorId: Int = 0,
+    val nombreJugador: String,
+    val email: String
 )
